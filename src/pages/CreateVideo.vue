2a4b85e90d67406513b2554081f796ef1fca0cc8
yangshuai <yangshuai@kezaihui.com>
2020-02-15 02:11:29 +0800

    fix: fix page jump

--- src/pages/CreateVideo.vue
+++ src/pages/CreateVideo.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div v-if="pageJump.toPage === 'createVideo'">
+    <div v-if="dataPageJump.toPage === 'createVideo'">
       <el-row align="middle" style="margin-bottom: 10px;" type="flex">
         <el-col :span="2">
           <div>关键词</div>
@@ -105,7 +105,7 @@
           v-if="unselectedVideo.count">
       </el-pagination>
     </div>
-    <EditVideo :pageJump="pageJump" :waitEditVideo="selectedVideo" v-else-if="pageJump.toPage === 'editVideo'"/>
+    <EditVideo :pageJump="pageJump" :waitEditVideo="selectedVideo" v-else-if="dataPageJump.toPage === 'editVideo'"/>
   </div>
 </template>
 
@@ -133,11 +133,75 @@ export default {
         count: 0
       },
       unselectedVideo: {
-        data: [],
+        data: [
+            {"uid": "a80d532dfd0846378cfcd3c87db4261c", "created_at": "2020-01-24T13:45:07.190269", "episode": 1473,
+             "subtile": "那会是很可惜的  主席先生\nif we allowed a few rotten apples to give a bad nameto the whole barrel.",
+             "subtile_sn": 1402, "begin_at": "02:22:16,250", "end_at": "02:22:18,750",
+             "video_url": "https://fangzhou-video.oss-cn-beijing.aliyuncs.com/oss:/app/oss/split/app/oss/download/movie/教父2-1402.mp4",
+             "audio_url": "https://fangzhou-video.oss-cn-beijing.aliyuncs.com/oss:/app/oss/split/app/oss/download/movie/教父2-1402.mp3",
+             "episode_name": "教父2-1402", },
+            {"uid": "d02b095a8a5d45c095b34249c8d29961", "created_at": "2020-01-24T13:38:52.839484", "episode": 1437,
+             "subtile": "P8爪钩？\nThe P-8 grapple hook?", "subtile_sn": 861, "begin_at": "01:12:54,130",
+             "end_at": "01:12:55,430",
+             "video_url": "https://fangzhou-video.oss-cn-beijing.aliyuncs.com/oss:/app/oss/split/app/oss/download/movie/憨豆特工2-861.mp4",
+             "audio_url": "https://fangzhou-video.oss-cn-beijing.aliyuncs.com/oss:/app/oss/split/app/oss/download/movie/憨豆特工2-861.mp3",
+             "episode_name": "憨豆特工2-861"},
+            {"uid": "0aa18c25b6d847eba677109b88a0d328", "created_at": "2020-01-24T13:10:12.910277", "episode": 1100,
+             "subtile": "我在吃焦糖苹果味的棒棒糖呢\nI am eating a caramel apple.", "subtile_sn": 765,
+             "begin_at": "00:47:03,720", "end_at": "00:47:05,180",
+             "video_url": "https://fangzhou-video.oss-cn-beijing.aliyuncs.com/oss:/app/oss/split/app/oss/download/movie/疯狂愚蠢的爱-765.mp4",
+             "audio_url": "https://fangzhou-video.oss-cn-beijing.aliyuncs.com/oss:/app/oss/split/app/oss/download/movie/疯狂愚蠢的爱-765.mp3",
+             "episode_name": "疯狂愚蠢的爱-765"},
+            {"uid": "11f57954fc324101b1590c44abf80b38", "created_at": "2020-01-22T12:44:09.967963", "episode": 2704,
+             "subtile": "是苹果上\nWell, at an apple.", "subtile_sn": 500, "begin_at": "00:49:35,181",
+             "end_at": "00:49:36,474",
+             "video_url": "https://fangzhou-video.oss-cn-beijing.aliyuncs.com/oss:/apps/oss/split/apps/oss/download/movie/饥饿游戏-500.mp4",
+             "audio_url": "https://fangzhou-video.oss-cn-beijing.aliyuncs.com/oss:/apps/oss/split/apps/oss/download/movie/饥饿游戏-500.mp3",
+             "episode_name": "饥饿游戏-500"},
+            {"uid": "7e791e58d0404f409d63328398e659f0", "created_at": "2020-01-22T12:43:56.003678", "episode": 2704,
+             "subtile": "当你射中苹果时他们什么反应？\nWhat did they do when you shot the apple?", "subtile_sn": 458,
+             "begin_at": "00:47:28,554", "end_at": "00:47:32,642",
+             "video_url": "https://fangzhou-video.oss-cn-beijing.aliyuncs.com/oss:/apps/oss/split/apps/oss/download/movie/饥饿游戏-458.mp4",
+             "audio_url": "https://fangzhou-video.oss-cn-beijing.aliyuncs.com/oss:/apps/oss/split/apps/oss/download/movie/饥饿游戏-458.mp3",
+             "episode_name": "饥饿游戏-458"},
+            {"uid": "8310aeedfeba447686fdeddffb9174fb", "created_at": "2020-01-22T12:19:27.370124", "episode": 2688,
+             "subtile": "我必须和着苹果酱吃\nI have to crush it up in my applesauce", "subtile_sn": 1006,
+             "begin_at": "00:51:15,940", "end_at": "00:51:18,110",
+             "video_url": "https://fangzhou-video.oss-cn-beijing.aliyuncs.com/oss:/apps/oss/split/apps/oss/download/movie/我的间谍前男友-1006.mp4",
+             "audio_url": "https://fangzhou-video.oss-cn-beijing.aliyuncs.com/oss:/apps/oss/split/apps/oss/download/movie/我的间谍前男友-1006.mp3",
+             "episode_name": "我的间谍前男友-1006"},
+            {"uid": "49ec0d705d1c4fb58531d51d7aaeba19", "created_at": "2020-01-22T12:06:20.908850", "episode": 2680,
+             "subtile": "我的侍女正拿着一个叫菠萝的东西过来\nand my maid is on her way up with something called a pineapple.",
+             "subtile_sn": 1262, "begin_at": "01:35:59,180", "end_at": "01:36:01,980",
+             "video_url": "https://fangzhou-video.oss-cn-beijing.aliyuncs.com/oss:/apps/oss/split/apps/oss/download/movie/宠儿-1262.mp4",
+             "audio_url": "https://fangzhou-video.oss-cn-beijing.aliyuncs.com/oss:/apps/oss/split/apps/oss/download/movie/宠儿-1262.mp3",
+             "episode_name": "宠儿-1262"},
+            {"uid": "a797c3a3ed8b438e9c7378fc5f0e16b9", "created_at": "2020-01-22T11:41:10.362485", "episode": 2671,
+             "subtile": "他们一定制造了数百万听\n松露，李子和苹果罐头。\nthey must have made millions of tins of P&A, plum and apple.",
+             "subtile_sn": 274, "begin_at": "00:14:32,900", "end_at": "00:14:37,190",
+             "video_url": "https://fangzhou-video.oss-cn-beijing.aliyuncs.com/oss:/apps/oss/split/apps/oss/download/movie/他们已不再变老-274.mp4",
+             "audio_url": "https://fangzhou-video.oss-cn-beijing.aliyuncs.com/oss:/apps/oss/split/apps/oss/download/movie/他们已不再变老-274.mp3",
+             "episode_name": "他们已不再变老-274"},
+            {"uid": "692b00cd3b02445aba0573c409661992", "created_at": "2020-01-22T11:41:08.892835", "episode": 2671,
+             "subtile": "递给他一听李子和苹果果酱。\nhanding him a tin of plum and apple jam.", "subtile_sn": 270,
+             "begin_at": "00:14:23,020", "end_at": "00:14:25,350",
+             "video_url": "https://fangzhou-video.oss-cn-beijing.aliyuncs.com/oss:/apps/oss/split/apps/oss/download/movie/他们已不再变老-270.mp4",
+             "audio_url": "https://fangzhou-video.oss-cn-beijing.aliyuncs.com/oss:/apps/oss/split/apps/oss/download/movie/他们已不再变老-270.mp3",
+             "episode_name": "他们已不再变老-270"},
+            {"uid": "4eb5510dc8c8444098284545be9c4673", "created_at": "2020-01-22T11:41:07.873997", "episode": 2671,
+             "subtile": "里面只有李子和苹果，你知道吗?\nbut plum and apple, you know?", "subtile_sn": 267,
+             "begin_at": "00:14:12,880", "end_at": "00:14:14,970",
+             "video_url": "https://fangzhou-video.oss-cn-beijing.aliyuncs.com/oss:/apps/oss/split/apps/oss/download/movie/他们已不再变老-267.mp4",
+             "audio_url": "https://fangzhou-video.oss-cn-beijing.aliyuncs.com/oss:/apps/oss/split/apps/oss/download/movie/他们已不再变老-267.mp3",
+             "episode_name": "他们已不再变老-267"}],
         count: 0,
         page: 0,
         pageSize: 3,
       },
+      dataPageJump: {
+        toPage: '',
+        beforePage: '',
+      }
     }
   },
   methods: {
@@ -156,14 +220,14 @@ export default {
       this.selectedVideo.count--;
     },
     handleNextStep() {
-      this.pageJump.toPage = 'editVideo';
-      this.pageJump.beforePage = 'createVideo';
+      this.dataPageJump.toPage = 'editVideo';
+      this.dataPageJump.beforePage = 'createVideo';
       // eslint-disable-next-line no-console
-      console.log(this.pageJump.toPage)
+      console.log(this.dataPageJump.toPage)
     },
     handleLastStep() {
-      this.pageJump.toPage = 'wordTheatre';
-      this.pageJump.beforePage = 'createVideo';
+      this.dataPageJump.toPage = 'wordTheatre';
+      this.dataPageJump.beforePage = 'createVideo';
     },
     handleSearch() {
       // 数据初始化
@@ -212,11 +276,15 @@ export default {
         })
     }
   },
+  mounted() {
+    this.dataPageJump.toPage = this.pageJump.toPage
+    this.dataPageJump.beforePage = this.pageJump.beforePage
+  },
   destroy() {
     // eslint-disable-next-line no-console
     console.log(this.selectedVideo);
     // eslint-disable-next-line no-console
-    console.log(this.pageJump);
+    console.log(this.dataPageJump);
   }
 }
 </script>
